1e801237d099cb17b00d89a5cdd2b6fb34074ebc
student <student@B23-14>
2018-04-18 12:31:13 +0200

    komentar

---
+++ MVCNajprejKoda/MVCNajprejKoda/Models/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCNajprejKoda.Models
+{
+    public class Film
+    {
+        public int id { get; set; }
+        public string Naslov { get; set; }
+        public DateTime LetoIzdaje { get; set; }
+        public string Tip { get; set; }
+        public decimal Cena { get; set; }
+    }
+}
