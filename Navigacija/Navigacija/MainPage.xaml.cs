e7981d7044fcc442e24d5635c2283649c14d1179
student <student@B23-14>
2018-05-16 12:34:23 +0200

    uwp

---
+++ Navigacija/Navigacija/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Navigacija
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            frame.Navigate(typeof(Page1));
+        }
+
+        private void btnDomov_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(typeof(Page1));
+        }
+
+        private void btnNazaj_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.CanGoBack)
+                frame.GoBack();
+        }
+
+        private void btnNaprej_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.CanGoForward)
+                frame.GoForward();
+        }
+
+        private void btnRoot_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page2));
+        }
+    }
+}
